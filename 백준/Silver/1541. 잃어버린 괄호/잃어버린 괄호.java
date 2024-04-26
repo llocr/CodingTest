e8949f27915f2333f69485ad69213dfce001b0dc
희수 <114149212+llocr@users.noreply.github.com>
2024-04-26 15:44:24 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,14 +1,16 @@
-import javax.swing.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     static int answer = 0;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String example = sc.nextLine();
-        String[] arr = example.split("-");
 
-        for(int i=0; i<arr.length; i++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] arr = s.split("-");
+
+        for(int i = 0; i<arr.length; i++) {
             int temp = mySum(arr[i]);
             if(i == 0) {
                 answer += temp;
@@ -16,14 +18,15 @@ public class Main {
                 answer -= temp;
             }
         }
+
         System.out.println(answer);
     }
 
     private static int mySum(String s) {
         int sum = 0;
         String[] str = s.split("[+]");
-        for(int i=0; i<str.length; i++){
-            sum += Integer.parseInt(str[i]);
+        for(int i = 0; i<str.length; i++) {
+            sum+= Integer.parseInt(str[i]);
         }
         return sum;
     }
