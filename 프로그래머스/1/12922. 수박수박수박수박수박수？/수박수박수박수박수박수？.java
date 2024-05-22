23bc1a2ac40433c560d8e1d719d6733fbdffe23f
희수 <114149212+llocr@users.noreply.github.com>
2024-05-22 09:08:02 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.06 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i<n/2; i++) {
+            sb.append("수박");
+        }
+        
+        if(n % 2 != 0) {
+            sb.append("수");
+        }    
+            
+        return sb.toString();
+    }
+}
