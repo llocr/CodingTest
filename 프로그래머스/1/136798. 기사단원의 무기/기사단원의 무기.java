81b3cc732924b559440f25aa3a9c4e909ca4f9af
희수 <114149212+llocr@users.noreply.github.com>
2024-06-20 09:22:19 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.72 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        int[] arr = new int[number+1];
+        
+        for(int i = 1; i<= number; i++) {
+            for(int j = 1; j<= Math.sqrt(i); j++) {
+                if(j*j == i) arr[i]++;
+                else if(i % j == 0) arr[i] += 2; 
+            }
+            if(arr[i] > limit) arr[i] = power;
+        }
+        
+        for(int i = 1; i<=number; i++) {
+            answer += arr[i];
+        }
+        
+        return answer;
+    }
+}
