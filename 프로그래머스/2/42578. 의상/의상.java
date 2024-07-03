bb2fdd5def3662242389abddd5de576297068ac8
희수 <114149212+llocr@users.noreply.github.com>
2024-07-03 15:34:26 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < clothes.length; i++) {
+            String key = clothes[i][1];
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        
+        for(String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        
+        return answer - 1;
+    }
+}
